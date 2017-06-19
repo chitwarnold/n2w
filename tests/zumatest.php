803058d63e8492709124b1c3280406005976ee02
Arnold Chitwa Astrill <chitwarnie@gmail.com>
2017-06-20 00:12:31 +0300

    Updated the video links

---
+++ tests/zumatest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vh1
+ * Date: 6/20/17
+ * Time: 12:10 AM
+ *
+ *
+ *
+ * will help out read the number that President JacobZuma was struggling to pronounce.
+ * @link https://www.youtube.com/watch?v=nqNa6992ih4
+ * @link https://www.youtube.com/watch?v=nHh9VjyyEYA
+ *
+ */
